645dc01aa7ca005d28d677d1c5e8d99b7f3818ce
JustZhenya <s404.gordon@gmail.com>
2024-11-07 16:17:13 +0300

    update fedora workflow, add fedora 41

--- make_rpm_package.sh
+++ make_rpm_package.sh
@@ -2,23 +2,26 @@
 
 # Create directory structure
 echo Create directory structure
-mkdir ~/rpmbuild
-mkdir ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-mkdir ~/rpmbuild/BUILDROOT/sdrpp-1.2.0$BUILD_NO-1.x86_64/
+rpmdev-setuptree
 
 # Create package info
 cat <<EOF >> ~/rpmbuild/SPECS/sdrpp.spec
 %global __requires_exclude libhackrf.so.0|libportaudio.so.2|librtlsdr.so.0|libfobos.so|libperseus-sdr.so.0|librfnm.so|libsdrplay_api.so.3
+%define _unpackaged_files_terminate_build 0
 
 Name:       sdrpp
-Version:    1.2.0$BUILD_NO
-Release:    1
+Version:    1.2.0
+Release:    $BUILD_NO
 Summary:    SDR++
 Recommends: libhackrf.so.0()(64bit), libportaudio.so.2()(64bit), librtlsdr.so.0()(64bit), libfobos.so()(64bit), libperseus-sdr.so.0()(64bit), librfnm.so()(64bit), libsdrplay_api.so.3()(64bit)
 License:    GPLv3+
 
 %description
 SDR++ is a cross-platform and open source SDR software with the aim of being bloat free and simple to use.
+
+%install
+cd /root/SDRPlusPlus/build
+%make_install
 
 %post
 /sbin/ldconfig
@@ -34,12 +37,6 @@ SDR++ is a cross-platform and open source SDR software with the aim of being blo
 /usr/share/sdrpp
 EOF
 
-# Copying files
-echo Copy files
-cd $1
-make install DESTDIR=~/rpmbuild/BUILDROOT/sdrpp-1.2.0$BUILD_NO-1.x86_64/
-
 # Create package
 echo Create package
-cd ~/rpmbuild/SPECS
 rpmbuild -ba sdrpp.spec
