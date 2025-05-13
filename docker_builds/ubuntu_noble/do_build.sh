184abdbaf1702e29e1144bfafcef44799f577906
JustZhenya <s404.gordon@gmail.com>
2025-05-14 02:21:36 +0300

    add arm64, also fix shit

--- docker_builds/ubuntu_noble/do_build.sh
+++ docker_builds/ubuntu_noble/do_build.sh
@@ -3,17 +3,17 @@ set -e
 cd /root
 
 # Install dependencies and tools
-apt update
-apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libvolk-dev libzstd-dev libairspyhf-dev libairspy-dev \
+apt-get update
+apt-get install -y build-essential cmake git libfftw3-dev libglfw3-dev libvolk-dev libzstd-dev libairspyhf-dev libairspy-dev \
             libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget portaudio19-dev \
             libcodec2-dev autoconf libtool xxd libspdlog-dev
 
 # Install SDRPlay libraries
-SDRPLAY_ARCH=$(dpkg --print-architecture)
+BUILD_ARCH=$(dpkg --print-architecture)
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.15.2.run
 7z x ./SDRplay_RSP_API-Linux-3.15.2.run
 7z x ./SDRplay_RSP_API-Linux-3.15.2
-cp $SDRPLAY_ARCH/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
+cp $BUILD_ARCH/libsdrplay_api.so.3.15 /usr/lib/libsdrplay_api.so
 cp inc/* /usr/include/
 
 # Install libperseus
@@ -21,7 +21,7 @@ git clone https://github.com/Microtelecom/libperseus-sdr
 cd libperseus-sdr
 autoreconf -i
 ./configure
-make
+make -j`nproc`
 make install
 ldconfig
 cd ..
@@ -32,7 +32,7 @@ cd librfnm
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
-make -j2
+make -j`nproc`
 make install
 cd ../../
 
@@ -42,7 +42,7 @@ cd libfobos
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
-make -j2
+make -j`nproc`
 make install
 cd ../../
 
@@ -50,8 +50,8 @@ cd SDRPlusPlus
 mkdir build
 cd build
 cmake .. -DOPT_BUILD_BLADERF_SOURCE=ON -DOPT_BUILD_LIMESDR_SOURCE=ON -DOPT_BUILD_SDRPLAY_SOURCE=ON -DOPT_BUILD_NEW_PORTAUDIO_SINK=ON -DOPT_BUILD_M17_DECODER=ON -DOPT_BUILD_PERSEUS_SOURCE=ON -DOPT_BUILD_RFNM_SOURCE=ON -DOPT_BUILD_FOBOSSDR_SOURCE=ON
-make VERBOSE=1 -j2
+make VERBOSE=1 -j`nproc`
 
 # why librtlsdr2???
 cd ..
-sh make_debian_package.sh ./build 'amd64' 'libc6, libgcc-s1, libstdc++6, libvolk3.1t64, libfftw3-single3, librtaudio6, libzstd1, libglfw3, libopengl0' 'libportaudio2, libad9361-0, libairspyhf1, libairspy0, libbladerf2, libcodec2-1.2, libhackrf0, libiio0, liblimesuite23.11-1, librtlsdr2'
+sh make_debian_package.sh ./build $BUILD_ARCH 'libc6, libgcc-s1, libstdc++6, libvolk3.1t64, libfftw3-single3, librtaudio6, libzstd1, libglfw3, libopengl0' 'libportaudio2, libad9361-0, libairspyhf1, libairspy0, libbladerf2, libcodec2-1.2, libhackrf0, libiio0, liblimesuite23.11-1, librtlsdr2'
